5f64a0d7a23f0e894ce0222b8e56952c407ce378
JohnKaramw <johnmaram400@gmail.com>
2025-08-22 16:14:48 +0300

    add styel manager

---
+++ lib/presentation/styles_manager.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+TextStyle _gettextStyle(double Fontsiz, FontWeight Fontweight, Color Color) {
+  return TextStyle(fontSize: Fontsiz, fontWeight: Fontweight, color: Color);
+}
